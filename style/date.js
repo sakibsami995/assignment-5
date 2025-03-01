ec00c7d1c38a6d2bb5a7e924e6f83dc77c735003
sakibsami995 <samisakib24@gmail.com>
2025-03-02 00:43:32 +0600

    added date

---
+++ style/date.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const dateToday = document.getElementById('current-date');
+    const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+    const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+    const today = new Date();
+    
+    const dayName = days[today.getDay()];
+    const monthName = months[today.getMonth()];
+    const date2 = today.getDate();
+    const year2 = today.getFullYear();
+    
+    dateToday.innerHTML = `${dayName}, <br> ${monthName} ${date2} ${year2}`;
+});
